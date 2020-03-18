afc62880129eaabc1f8af4bed371b2adcb06591c
Magicianred <magicianred@gmail.com>
2020-03-18 12:35:04 +0100

    First commit

---
+++ src/__tests__/Components/Pages/WhoAre.spec.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+import { create } from 'react-test-renderer';
+
+import WhoAre from "./../../../Components/Pages/WhoAre";
+
+let container: Element;
+beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container);
+});
+
+afterEach(() => {
+    unmountComponentAtNode(container);
+    container.remove();
+});
+
+describe("WhoAre Component", () => {
+
+    describe("Snapshots", () => {
+    
+        it("Matches the snapshot (with default title)", () => {
+            const instance = create(<WhoAre />);
+            expect(instance.toJSON()).toMatchSnapshot();
+        });
+    
+    });
+
+    describe("Check content elements", () => {
+        it("Render Component", () => {
+            act(() => {
+                render(<WhoAre />, container);
+            });
+            expect(container.querySelector("h2").textContent).toBe("Who Are");
+            expect(container.querySelector("p").textContent).toBe("This is the who are page");
+        });
+    });
+});
